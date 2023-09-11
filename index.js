f5dac3e16fb66c9d8d4c6097058069a58866ac37
duyviet2101 <117768320+duyviet2101@users.noreply.github.com>
2023-09-12 00:51:42 +0700

    add get page

--- index.js
+++ index.js
@@ -12,9 +12,16 @@ const requestNotion = axios.create({
     }
 })
 
+//!config urlencoded
+app.use(
+    express.urlencoded({
+        extended: true,
+    }),
+); //TH gửi thông tin qua dạng form html
+app.use(express.json()); // TH gửi từ code JS
 
 
-app.get('/database/:databaseId', async (req, res) => {
+app.post('/databases/:databaseId/query', async (req, res) => {
     const databaseId = req.params.databaseId;
     try {
         //! get thông tin của database
@@ -33,7 +40,6 @@ app.get('/database/:databaseId', async (req, res) => {
             properties: responseDatabase.properties,
             parent: responseDatabase.parent
         }
-        
         //! get thông tin của các pages trong database
         const responsePages = await requestNotion({
             method: 'POST',
@@ -58,19 +64,68 @@ app.get('/database/:databaseId', async (req, res) => {
         database.data = blocks;
         res.send(database)
     } catch (error) {
-        console.log(error)
         if(error.respone){
-            res.send({error: error.respone})
-        } 
+            console.log('response>>>',error.respone)
+            res.send({error: error.respone.data})
+        } else
         if (error.request) {
-            res.send({error: error.request})
-        }
+            console.log('request>>>',error.request.data)
+            res.send({error: `request error`})
+        } else
         if (error.message) {
+            console.log('message>>>',error.message)
             res.send({error: error.message})
-        }
+        } else
         res.send({error: "UNKNOWN ERROR"})
     }
 
+})
+
+app.get('/pages/:pageId', async (req, res) => {
+    const pageId = req.params.pageId;
+    try {
+        //! get thông tin của page
+        var responsePage = await requestNotion({
+            method: 'GET',
+            url: `/pages/${pageId}`,
+        }).then((response) => response.data)
+        
+        const page = {
+            object: responsePage.object,
+            id: responsePage.id,
+            created_time: responsePage.created_time,
+            last_edited_time: responsePage.last_edited_time,
+            properties: responsePage.properties,
+            parent: responsePage.parent
+        }
+
+        //! get blocks in page
+        const blocks =  await requestNotion({
+            method: 'GET',
+            url: `/blocks/${pageId}/children?page_size=100`
+        }).then(response => response.data)
+        page.data = blocks;
+        res.send(page)
+    } catch (error) {
+        if(error.respone){
+            console.log('response>>>',error.respone)
+            res.send({error: error.respone.data})
+        } else
+        if (error.request) {
+            console.log('request>>>',error.request.data)
+            res.send({error: `request error`})
+        } else
+        if (error.message) {
+            console.log('message>>>',error.message)
+            res.send({error: error.message})
+        } else
+        res.send({error: "UNKNOWN ERROR"})
+    }
+
+})
+
+app.get('/', (req, res) => {
+    res.send('<a href="https://github.com/duyviet2101/trash/tree/master#readme">Supported APIs</a>')
 })
 
 app.listen(3005, () => {
