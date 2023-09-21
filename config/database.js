2c979d73c07f435200f9bbd00a3118b4e663a3da
duyviet2101 <117768320+duyviet2101@users.noreply.github.com>
2023-09-21 23:18:39 +0700

    add connection string

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 module.exports.connect = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URL)
+        await mongoose.connect("mongodb://duyviet2101:123456@101.96.66.219:8000/codelab1?authMechanism=DEFAULT&authSource=codelab1")
         console.log('DATABASE CONNECTED!!!')
     } catch (error) {
         console.log('Cant connect to database!!!')
